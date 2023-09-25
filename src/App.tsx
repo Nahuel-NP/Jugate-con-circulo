871e05e57a583a9f549868aded99ca13af8ebb4e
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-09-25 18:51:26 -0300

    init project with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <main className="bg-slate-900 min-h-screen grid place-items-center">
+      <h1 className="text-white text-3xl font-bold">Hello world</h1>
+    </main>
+  )
+}
+
+export default App
