b71cf7e8b6a57a34aadf4965b16922e98ab3e100
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-10 13:28:37 -0300

    fix mobile rocket size

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <main className="h-screen bg-center bg-cover bg-montana font-rubik">
-      <div style={{ viewTransitionName: 'rocket' }} className="absolute top-0 left-0 z-50 grid w-full h-20 grid-cols-2 before:border-b-8 before:border-c-cyan before:border-dotted before:relative before:col-start-2">
-        <img src="/images/rocket.png" alt="rocket" className="absolute z-50 w-full max-w-[200px] -translate-x-1/2 rocket-animation top-1/2 left-1/2" />
+      <div style={{ viewTransitionName: 'rocket' }} className="absolute top-0 left-0 z-50 grid w-full h-16 grid-cols-2 md:h-20 before:border-b-8 before:border-c-cyan before:border-dotted before:relative before:col-start-2">
+        <img src="/images/rocket.png" alt="rocket" className="absolute z-50 w-full md:max-w-[200px] max-w-[150px] -translate-x-1/2 rocket-animation top-1/2 left-1/2" />
       </div>
         <Outlet />
       <img style={{ viewTransitionName: 'mountains' }} src="/images/backgrounds/montana/front.png" className="fixed bottom-0 z-10 w-full " loading="lazy" decoding="async" alt="mountains" />
