83e0ef996596f51ef9b07af01ae09e613c01ba6d
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-10 14:46:40 -0300

    add saturation

---
+++ src/pages/Game.tsx
@@ -0,0 +1,11 @@
+
+
+const Game = () => {
+  return (
+    < section className="flex items-center justify-center w-full h-full pt-16 md:pt-4 saturate-150" style={{ viewTransitionName: 'view', background: 'linear-gradient(90deg, rgba(53, 19, 96, 0.50) 0%, rgba(29, 183, 179, 0.50) 100%)' }}>
+      <h1>game</h1>
+    </section>
+  );
+}
+
+export default Game;
