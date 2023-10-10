88f7ba2ab5748c2384659e5ab9fc68160852f665
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-10 16:04:52 -0300

    add overlay for instruction

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <main className="h-screen bg-center bg-cover bg-montana font-rubik">
+    <main className="relative h-screen bg-center bg-cover bg-montana font-rubik">
       <div style={{ viewTransitionName: 'rocket' }} className="absolute top-0 left-0 z-50 grid w-full h-16 grid-cols-2 lg:h-20 before:border-b-8 before:border-c-cyan before:border-dotted before:relative before:col-start-2">
         <img src="/images/rocket.png" alt="rocket" className="absolute z-50 w-full lg:max-w-[200px] max-w-[150px] -translate-x-1/2 rocket-animation top-1/2 left-1/2" />
       </div>
-        <Outlet />
+      <Outlet />
       <img style={{ viewTransitionName: 'mountains' }} src="/images/backgrounds/montana/front.png" className="fixed bottom-0 z-10 w-full " loading="lazy" decoding="async" alt="mountains" />
+      <div className="absolute top-0 left-0 backdrop-blur-sm !z-50 w-full h-full bg-opacity-70 bg-c-purple flex flex-col justify-center items-center gap-10">
+        <p className="w-[324px] h-[99px] text-center text-white text-base font-bold font-['Rubik'] leading-[31px]">Para elegir un Circulerx seleccioná y arrastrá hasta la tarjeta con la descripción</p>
+        <img src="/images/icons/swipe-icon.svg" alt="swap icon" className="animate-wiggle-more animate-infinite animate-duration-[2000ms] animate-delay-0 animate-ease-in-out animate-normal animate-fill-both" />
+        <button className="w-[153px] h-[47px] bg-yellow-400 rounded-[50px] hover:scale-105 transition-transform shadow">Entendido</button>
+      </div>
     </main>
   );
 }
