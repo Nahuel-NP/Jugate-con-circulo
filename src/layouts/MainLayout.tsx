aa0f6d3e221462e0ece13a1adca64ba3626dd072
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-10 13:26:55 -0300

    fix mobile height

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <main className="min-h-screen bg-center bg-cover bg-montana font-rubik">
+    <main className="h-screen bg-center bg-cover bg-montana font-rubik">
       <div style={{ viewTransitionName: 'rocket' }} className="absolute top-0 left-0 z-50 grid w-full h-20 grid-cols-2 before:border-b-8 before:border-c-cyan before:border-dotted before:relative before:col-start-2">
         <img src="/images/rocket.png" alt="rocket" className="absolute z-50 w-full max-w-[200px] -translate-x-1/2 rocket-animation top-1/2 left-1/2" />
       </div>
