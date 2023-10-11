3fde848045ccbafc5d958b98a9da3c78e3b58d16
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-11 14:37:15 -0300

    add temp data

---
+++ src/data/circuleros.ts
@@ -0,0 +1,318 @@
+export interface Circulero {
+  id?: number,
+  name: string,
+  image: string,
+  roles: string[]
+
+}
+
+export const circuleros: Circulero[] = [
+  { id:1,
+    name: "Aguirre Jeremias",
+    image: "img-jere",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:2,
+    name: "Alarcon Mariana",
+    image: "img-maki",
+    roles: [
+      "DP"
+    ]
+  },
+  { id:3,
+    name: "Avalos Ariel",
+    image: "",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:4,
+    name: "BARBERO JUAN PABLO",
+    image: "",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:5,
+    name: "Castillo Gonzalo Emanuel",
+    image: "img-gonza",
+    roles: [
+      "IT"
+    ]
+  },
+  { id:6,
+    name: "Diaz Renzo",
+    image: "img-renzo",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:7,
+    name: "Garcia Ivette Gaia",
+    image: "",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:8,
+    name: "Garcia Malbran Natalia",
+    image: "",
+    roles: [
+      "RRHH"
+    ]
+  },
+  { id:9,
+    name: "Godoy Brenda",
+    image: "img-godoy-brenda",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:10,
+    name: "Guzman Viviana",
+    image: "img-vivs",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:11,
+    name: "Hefling Facundo",
+    image: "img-facu",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:12,
+    name: "Jones Andreina",
+    image: "img-andy",
+    roles: [
+      "DP"
+    ]
+  },
+  { id:13,
+    name: "Kek Danisa",
+    image: "img-danisa",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:14,
+    name: "Lievano Fernando",
+    image: "img-fer-lievano",
+    roles: [
+      "IT"
+    ]
+  },
+  { id:15,
+    name: "Marin Francisco",
+    image: "",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:16,
+    name: "Martinez Enzo",
+    image: "img-enzo",
+    roles: [
+      "MARKETING"
+    ]
+  },
+  { id:17,
+    name: "Matteucci Jordana",
+    image: "img-jordy",
+    roles: [
+      "DP"
+    ]
+  },
+  { id:18,
+    name: "Mihof Lisandro",
+    image: "img-lisandro",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:19,
+    name: "Milan Roy",
+    image: "img-roy",
+    roles: [
+      "DP"
+    ]
+  },
+  { id:20,
+    name: "Nuñez Pablo",
+    image: "img-pablo",
+    roles: [
+      "IT"
+    ]
+  },
+  { id:21,
+    name: "Olivello Aldana",
+    image: "img-aldi",
+    roles: [
+      "PRODUCCION AUDIOVISUAL"
+    ]
+  },
+  { id:22,
+    name: "Pedroso Nahuel",
+    image: "img-nahuel",
+    roles: [
+      "IT"
+    ]
+  },
+  { id:23,
+    name: "Rangel Valeska",
+    image: "img-valeska",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:24,
+    name: "Talavan Juanpi",
+    image: "img-tala",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:25,
+    name: "Toloza Augusto",
+    image: "img-augusto",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:26,
+    name: "Vivoli Lucia",
+    image: "img-luli",
+    roles: [
+      "ADMIN"
+    ]
+  },
+  { id:27,
+    name: "Sarrua Gaston",
+    image: "img-gaston",
+    roles: [
+      "DP"
+    ]
+  },
+  { id:28,
+    name: "Brea Alejandra",
+    image: "img-ale-brea",
+    roles: [
+      "MARKETING"
+    ]
+  },
+  { id:29,
+    name: "Mumare Natalia",
+    image: "img-mumare",
+    roles: [
+      "ADMIN"
+    ]
+  },
+  { id:30,
+    name: "Sleiman Parra Valentina",
+    image: "img-parra-valen",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:31,
+    name: "Mendez Matias",
+    image: "img-mati",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:32,
+    name: "Fernandez Soledad",
+    image: "img-sol",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:33,
+    name: "Alunni Juan Ignacio",
+    image: "",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:34,
+    name: "Florentin Lara",
+    image: "img-lara",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:35,
+    name: "Romero Maria Eugenia",
+    image: "img-maru",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:36,
+    name: "Maidana Maria Lourdes",
+    image: "",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:37,
+    name: "Zacariaz Victoria",
+    image: "img-vico",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:38,
+    name: "Sponton Valentina",
+    image: "img-valen-sponton",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:39,
+    name: "Robledo Micaela",
+    image: "img-mica-robledo",
+    roles: [
+      "DISEÑO"
+    ]
+  },
+  { id:40,
+    name: "Tello Aldana",
+    image: "img-aldi-tello",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  },
+  { id:41,
+    name: "Magariños Andres Manuel",
+    image: "",
+    roles: [
+      "CREATIVIDAD"
+    ]
+  },
+  { id:42,
+    name: "Durand Santiago",
+    image: "",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:43,
+    name: "Vicente Agustina",
+    image: "",
+    roles: [
+      "CUENTAS"
+    ]
+  },
+  { id:44,
+    name: "Iabichella Robert Agustin",
+    image: "",
+    roles: [
+      "AUDIOVISUAL"
+    ]
+  }
+]
