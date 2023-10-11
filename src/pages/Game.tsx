d5131be1b9b688506c4af4fc2d0938837e7415bd
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-11 15:22:19 -0300

    min-h and fix media query

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSensor, MouseSensor, TouchSensor, useSensors, DragEndEvent, DndContext, closestCenter } from '@dnd-kit/core';
 import { Circulero, circuleros } from "../data/circuleros";
 import { Droppable } from "../components/DragAndDrop/Droppable";
+import { Draggable } from "../components/DragAndDrop/Draggable";
 
 
 
@@ -66,27 +67,29 @@ const Game = () => {
   }
 
 
-
-
-
   return (
-    < section className="flex items-center justify-center w-full h-full pt-16 md:pt-4 saturate-150" style={{ viewTransitionName: 'view', background: 'linear-gradient(90deg, rgba(53, 19, 96, 0.50) 0%, rgba(29, 183, 179, 0.50) 100%)' }}>
-      <div className="container">
+    < section className="z-50 flex items-center justify-center w-full min-h-screen py-16 lg:p-8 md:pt-4 saturate-150" style={{ viewTransitionName: 'view', background: 'linear-gradient(90deg, rgba(53, 19, 96, 0.50) 0%, rgba(29, 183, 179, 0.50) 100%)' }}>
+      <div className="container py-8 md:mt-14">
         <DndContext sensors={sensors} autoScroll={false} collisionDetection={closestCenter} onDragEnd={drawEnd}>
 
-          <div className="grid gap-8 text-sm">
-            <div className="bg-c-magenta  relative min-h-[150px] max-w-xs  mx-auto md:max-w-sm before:w-full before:h-full before:absolute before:bg-white before:-z-10 before:-left-2 before:top-2 before:shadow-md before:shadow-black">{/* seleccion 1 */}
-              <Droppable id="one" >
-
-
+          <div className="grid max-w-6xl gap-4 mx-auto text-sm lg:gap-8 lg:grid-cols-5">
+            <div className="bg-c-magenta lg:col-start-1 lg:col-span-2  relative min-h-[150px] max-w-xs  mx-auto md:max-w-sm before:w-full before:h-full before:absolute before:bg-white before:-z-10 before:-left-2 before:top-2 before:shadow-md before:shadow-black">{/* seleccion 1 */}
+              <Droppable id="first" >
                 <p className="text-white">Un/a DP se encarga de dirigir la estrategia general del proyecto. Es responsable de armar el equipo, crear la propuesta y darle seguimiento a los avances.
                 </p>
               </Droppable>
             </div>
-            <div className="grid grid-cols-5">{/* grilla */}
+            <div className="lg:col-start-3 lg:row-span-2 lg:col-span-3">
+              <Droppable id="grid">
+                <div className="grid max-w-sm grid-cols-5 gap-2 lg:max-w-lg xl:max-w-2xl">
+                  {circuleros.map(item => (              
+                    <Draggable disabled={item == selectedCirculero.firstCirculero || selectedCirculero.secondCirculero == item} id={item.id.toString()} key={item.id} circulero={item} />))
+                  }
+                </div>
+              </Droppable>
             </div>
-            <div className="bg-c-cyan  relative min-h-[150px] max-w-xs  mx-auto md:max-w-sm before:w-full before:h-full before:absolute before:bg-white before:-z-10 before:-left-2 before:top-2 before:shadow-md before:shadow-black">{/* seleccion 2 */}
-              <Droppable id="one" >
+            <div className="bg-c-cyan lg:col-start-1  lg:col-span-2 relative min-h-[150px] max-w-xs  mx-auto md:max-w-sm before:w-full before:h-full before:absolute before:bg-white before:-z-10 before:-left-2 before:top-2 before:shadow-md before:shadow-black">{/* seleccion 2 */}
+              <Droppable id="second" >
                 <p className="text-black">
                   Un/a CUENTAS es su mano derecha. Acompaña en la organización interna y en la comunicación con el cliente para solicitar información.
                 </p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   }
 
   return (
-    <div className="relative z-10 flex flex-col items-center justify-center w-full h-full gap-10 bg-center bg-cover saturate-150" style={{ viewTransitionName: 'view', background: 'linear-gradient(90deg, rgba(29, 183, 179, 0.50) 0%, rgba(45, 169, 182, 0.50) 19%, rgba(88, 132, 188, 0.50) 56%, rgba(145, 83, 197, 0.50) 100% )' }}>
+    <div className="relative z-10 flex flex-col items-center justify-center w-full min-h-screen gap-10 bg-center bg-cover saturate-150" style={{ viewTransitionName: 'view', background: 'linear-gradient(90deg, rgba(29, 183, 179, 0.50) 0%, rgba(45, 169, 182, 0.50) 19%, rgba(88, 132, 188, 0.50) 56%, rgba(145, 83, 197, 0.50) 100% )' }}>
         <img src="/images/title.svg" alt="Jugate Con Circulo" className="w-full max-w-xs md:max-w-xl" />
       <button onClick={() => handleRoute('/situation')} className="px-4 py-2 text-lg font-bold text-black uppercase transition-transform rounded-md hover:scale-105 bg-c-yellow">Iniciar</button>
     </div>
