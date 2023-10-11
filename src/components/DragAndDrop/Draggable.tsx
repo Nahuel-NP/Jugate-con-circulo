be2f95cb0d37bac2843b7142005d56591549d6fb
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-11 15:21:49 -0300

    draggable circulero

--- src/components/DragAndDrop/Draggable.tsx
+++ src/components/DragAndDrop/Draggable.tsx
@@ -1,7 +1,8 @@
 
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
-import { Circulero } from '../../App';
+import { Circulero } from '../../data/circuleros';
+
 
 interface Props {
   id: string;
@@ -22,18 +23,20 @@ export const Draggable = ({ id, circulero, disabled = false }: Props) => {
   });
   const style = {
     // Outputs `translate3d(x, y, 0)`
-    opacity: isDragging ? 1 : 0.6,
-
+    opacity: isDragging ? 1 : 0.9,
     transform: CSS.Translate.toString(transform),
   };
 
   return (
-    <div ref={setNodeRef} style={style}  {...listeners} {...attributes} className='flex items-center justify-center w-full bg-purple-400 rounded-full cursor-grab aspect-square'>
-      {disabled ? (<div className='w-full text-center cursor-not-allowed'>
-        <p className='text-xs bg-red-500 select-none md:text-base'>{circulero?.name}</p>
-      </div>) :
-        <div className='w-full text-center '>
-          <p className='text-xs select-none md:text-base'>{circulero?.name}</p>
+    <div ref={setNodeRef} style={style}  {...listeners} {...attributes} className='flex max-w-[100px] z-50 items-center justify-center w-full overflow-hidden rounded-full  cursor-grab aspect-square'>
+      {disabled
+        ?
+        <div className='w-full text-center cursor-not-allowed'>
+          <p className='text-xs bg-red-500 select-none md:text-base'>{circulero?.name}</p>
+        </div>
+        :
+        <div className='w-full p-1 md:p-2 bg-gradient-to-r from-c-cyan to-c-purple'>
+          <img className='rounded-full' src={`/images/circuleros/${circulero?.image ? circulero.image : 'no-profile'}.jpg`} alt={circulero?.name} />
         </div>}
     </div>
   );
