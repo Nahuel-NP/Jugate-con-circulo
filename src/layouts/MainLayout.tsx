1520d9bcc051cf596ebd2212fd95529674799dbf
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-11 15:21:06 -0300

    layout fixed background

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <main className="relative h-screen bg-center bg-cover bg-montana font-rubik">
+    <main className="relative bg-fixed bg-center bg-cover bg-montana font-rubik">
       <div style={{ viewTransitionName: 'rocket' }} className="absolute top-0 left-0 z-50 grid w-full h-16 grid-cols-2 lg:h-20 before:border-b-8 before:border-c-cyan before:border-dotted before:relative before:col-start-2">
         <img src="/images/rocket.png" alt="rocket" className="absolute z-50 w-full lg:max-w-[200px] max-w-[150px] -translate-x-1/2 rocket-animation top-1/2 left-1/2" />
       </div>
