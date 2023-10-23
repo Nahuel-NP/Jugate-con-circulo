e3fc2fa0b02859388ce61b2a784d9f4d76e14988
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-10-23 15:01:43 -0300

    update info circuleros

--- src/data/circuleros.ts
+++ src/data/circuleros.ts
@@ -1,318 +1,429 @@
 export interface Circulero {
   id: number,
   name: string,
+  apodo: string,
   image: string,
-  roles: string[]
-
+  roles: Array<
+  "Audiovisualista"
+  | "DP" 
+  | "Fotógrafo" 
+  | "Redactor" 
+  | "Cuentas" 
+  | "Desarrollador IT" 
+  | "Diseñador/a IT" 
+  | "Planner IT" 
+  | "Planner"
+  | "Planner de Pauta"
+  | "Administrativo/a"
+  | "Director/a de Arte"
+  | "Productor/a Audiovisual"
+  | "Contenidista de Marketing"
+  | "Contenidista"
+  | "Data Analyst en Marketing"
+  | "de Recursos Humanos"
+  | "Socio"
+  | "Diseñador/a"
+  | "Director/a de Creatividad">
 }
 
+
+
 export const circuleros: Circulero[] = [
-  { id:1,
+  {
+    id: 1,
     name: "Aguirre Jeremias",
+    apodo: "Jere",
     image: "img-jere",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
   },
-  { id:2,
+  {
+    id: 2,
     name: "Alarcon Mariana",
+    apodo: "Maki",
     image: "img-maki",
     roles: [
-      "DP"
+      "DP",
     ]
   },
-  { id:3,
+  {
+    id: 3,
     name: "Avalos Ariel",
+    apodo: "Ari",
     image: "",
     roles: [
-      "AUDIOVISUAL"
+      "Fotógrafo"
     ]
   },
-  { id:4,
+  {
+    id: 4,
     name: "BARBERO JUAN PABLO",
+    apodo: "Juanpa",
     image: "",
     roles: [
-      "CREATIVIDAD"
+      "Redactor"
     ]
   },
-  { id:5,
+  {
+    id: 5,
     name: "Castillo Gonzalo Emanuel",
+    apodo: "Gonza",
     image: "img-gonza",
     roles: [
-      "IT"
+      "Desarrollador IT","Diseñador/a"
     ]
   },
-  { id:6,
+  {
+    id: 6,
     name: "Diaz Renzo",
+    apodo: "Ren",
     image: "img-renzo",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:7,
+  {
+    id: 7,
     name: "Garcia Ivette Gaia",
+    apodo: "Ivette",
     image: "",
     roles: [
-      "CUENTAS"
+      "Cuentas","Diseñador/a"
     ]
   },
-  { id:8,
+  {
+    id: 8,
     name: "Garcia Malbran Natalia",
+    apodo: "Nati G",
     image: "",
     roles: [
-      "RRHH"
+      "de Recursos Humanos"
     ]
   },
-  { id:9,
+  {
+    id: 9,
     name: "Godoy Brenda",
+    apodo: "Bren",
     image: "img-godoy-brenda",
     roles: [
-      "CUENTAS"
+      "Cuentas"
     ]
   },
-  { id:10,
+  {
+    id: 10,
     name: "Guzman Viviana",
+    apodo: "Vivis",
     image: "img-vivs",
     roles: [
-      "CREATIVIDAD"
+      "Director/a de Creatividad"
     ]
   },
-  { id:11,
+  {
+    id: 11,
     name: "Hefling Facundo",
+    apodo: "Facu",
     image: "img-facu",
     roles: [
-      "CUENTAS"
+      "Cuentas"
     ]
   },
-  { id:12,
+  {
+    id: 12,
     name: "Jones Andreina",
+    apodo: "Andy",
     image: "img-andy",
     roles: [
       "DP"
     ]
   },
-  { id:13,
+  {
+    id: 13,
     name: "Kek Danisa",
+    apodo: "Dabus",
     image: "img-danisa",
     roles: [
-      "DISEÑO"
+      "Diseñador/a IT"
     ]
   },
-  { id:14,
+  {
+    id: 14,
     name: "Lievano Fernando",
+    apodo: "Fernandito",
     image: "img-fer-lievano",
     roles: [
-      "IT"
+      "Desarrollador IT"
     ]
   },
-  { id:15,
+  {
+    id: 15,
     name: "Marin Francisco",
-    image: "",
+    apodo: "Fachy",
+    image: "img-francisco",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:16,
+  {
+    id: 16,
     name: "Martinez Enzo",
+    apodo: "Enzo",
     image: "img-enzo",
     roles: [
-      "MARKETING"
+      "Planner de Pauta"
     ]
   },
-  { id:17,
+  {
+    id: 17,
     name: "Matteucci Jordana",
+    apodo: "Jordi",
     image: "img-jordy",
     roles: [
       "DP"
     ]
   },
-  { id:18,
+  {
+    id: 18,
     name: "Mihof Lisandro",
+    apodo: "Jejo",
     image: "img-lisandro",
     roles: [
-      "CREATIVIDAD"
+      "Director/a de Arte"
     ]
   },
-  { id:19,
+  {
+    id: 19,
     name: "Milan Roy",
+    apodo: "Roy",
     image: "img-roy",
     roles: [
-      "DP"
+      "DP","Diseñador/a"
     ]
   },
-  { id:20,
+  {
+    id: 20,
     name: "Nuñez Pablo",
+    apodo: "Pablo",
     image: "img-pablo",
     roles: [
-      "IT"
+      "Planner IT","Desarrollador IT"
     ]
   },
-/*   { id:21,
+  {
+    id: 21,
     name: "Olivello Aldana",
+    apodo: "Aldo",
     image: "img-aldi",
     roles: [
-      "PRODUCCION AUDIOVISUAL"
+      "Productor/a Audiovisual"
     ]
   },
-  { id:22,
+  {
+    id: 22,
     name: "Pedroso Nahuel",
+    apodo: "Nawe",
     image: "img-nahuel",
     roles: [
-      "IT"
+      "Desarrollador IT"
     ]
   },
-  { id:23,
+  {
+    id: 23,
     name: "Rangel Valeska",
+    apodo: "Valesk",
     image: "img-valeska",
     roles: [
-      "CREATIVIDAD"
+      "Contenidista","Planner"
     ]
   },
-  { id:24,
+  {
+    id: 24,
     name: "Talavan Juanpi",
+    apodo: "Tala",
     image: "img-tala",
     roles: [
-      "CREATIVIDAD"
+      "Director/a de Creatividad"
     ]
   },
-  { id:25,
+  {
+    id: 25,
     name: "Toloza Augusto",
+    apodo: "August",
     image: "img-augusto",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:26,
+  {
+    id: 26,
     name: "Vivoli Lucia",
+    apodo: "Luli",
     image: "img-luli",
     roles: [
-      "ADMIN"
+      "Administrativo/a"
     ]
   },
-  { id:27,
-    name: "Sarrua Gaston",
+  {
+    id: 27,
+    name: "Sarrua Gastón",
+    apodo: "Gastón",
     image: "img-gaston",
     roles: [
       "DP"
     ]
   },
-  { id:28,
+  {
+    id: 28,
     name: "Brea Alejandra",
+    apodo: "Ale",
     image: "img-ale-brea",
     roles: [
-      "MARKETING"
+      "Data Analyst en Marketing"
     ]
   },
-  { id:29,
+  {
+    id: 29,
     name: "Mumare Natalia",
+    apodo: "Nati",
     image: "img-mumare",
     roles: [
-      "ADMIN"
+      "Administrativo/a"
     ]
   },
-  { id:30,
+  {
+    id: 30,
     name: "Sleiman Parra Valentina",
+    apodo: "Valen Parra",
     image: "img-parra-valen",
     roles: [
-      "CREATIVIDAD"
+      "Contenidista de Marketing"
     ]
   },
-  { id:31,
+  {
+    id: 31,
     name: "Mendez Matias",
+    apodo: "Mati",
     image: "img-mati",
     roles: [
-      "CUENTAS"
+      "Cuentas"
     ]
   },
-  { id:32,
+  {
+    id: 32,
     name: "Fernandez Soledad",
+    apodo: "Sole",
     image: "img-sol",
     roles: [
-      "CREATIVIDAD"
+      "Director/a de Arte"
     ]
   },
-  { id:33,
+  {
+    id: 33,
     name: "Alunni Juan Ignacio",
+    apodo: "Alunni",
     image: "",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
   },
-  { id:34,
+  {
+    id: 34,
     name: "Florentin Lara",
+    apodo: "Lara",
     image: "img-lara",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
   },
-  { id:35,
+  {
+    id: 35,
     name: "Romero Maria Eugenia",
+    apodo: "Maru",
     image: "img-maru",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
   },
-  { id:36,
-    name: "Maidana Maria Lourdes",
-    image: "",
-    roles: [
-      "AUDIOVISUAL"
-    ]
-  },
-  { id:37,
+
+  {
+    id: 36,
     name: "Zacariaz Victoria",
+    apodo: "Vick",
     image: "img-vico",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:38,
+  {
+    id: 37,
     name: "Sponton Valentina",
+    apodo: "Valen Sponton",
     image: "img-valen-sponton",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:39,
+  {
+    id: 38,
     name: "Robledo Micaela",
+    apodo: "Mica",
     image: "img-mica-robledo",
     roles: [
-      "DISEÑO"
+      "Diseñador/a"
     ]
   },
-  { id:40,
+  {
+    id: 39,
     name: "Tello Aldana",
+    apodo: "Aldana T",
     image: "img-aldi-tello",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
   },
-  { id:41,
+  {
+    id: 40,
     name: "Magariños Andres Manuel",
+    apodo: "Andrés",
     image: "",
     roles: [
-      "CREATIVIDAD"
+      "Redactor","Director/a de Arte"
     ]
   },
-  { id:42,
+  {
+    id: 41,
     name: "Durand Santiago",
+    apodo: "Santi",
     image: "",
     roles: [
-      "CUENTAS"
+      "Cuentas","Audiovisualista"
     ]
   },
-  { id:43,
+  {
+    id: 42,
     name: "Vicente Agustina",
+    apodo: "Agus",
     image: "",
     roles: [
-      "CUENTAS"
+      "Cuentas"
     ]
   },
-  { id:44,
+  {
+    id: 43,
     name: "Iabichella Robert Agustin",
+    apodo: "Agustin",
     image: "",
     roles: [
-      "AUDIOVISUAL"
+      "Audiovisualista"
     ]
-  } */
+  },
+  {
+    id: 43,
+    name: "Iabichella Robert Agustin",
+    apodo: "Kike",
+    image: "img-kike",
+    roles: [
+      "Socio","Diseñador/a IT","Diseñador/a"
+    ]
+  }
 ]
