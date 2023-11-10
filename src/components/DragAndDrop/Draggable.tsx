c66fd52b008d06e8a638a333ec8da53b61f26bd5
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-11-10 12:44:16 -0300

    fix draggable text size

--- src/components/DragAndDrop/Draggable.tsx
+++ src/components/DragAndDrop/Draggable.tsx
@@ -32,8 +32,8 @@ export const Draggable = ({ id, circulero, disabled = false }: Props) => {
     <div ref={setNodeRef} style={style}  {...listeners} {...attributes} className='flex max-w-[70px] xl:max-w-[80px] 2xl:max-w-[90px] z-50 items-center animate-fade animate-once animate-duration-1000 animate-ease-in-out animate-normal animate-fill-forwards justify-center w-full overflow-hidden rounded-full  cursor-grab aspect-square'>
       {disabled
         ?
-        <div className='flex items-center justify-center w-full h-full text-center border-4 lg:border-[6px] rounded-full cursor-not-allowed border-c-cyan'>
-          <p className='text-[10px] leading-[12px] px-2 text-white select-none sm:text-xs xl:text-base uppercase '>{circulero?.apodo}</p>
+        <div className='flex items-center justify-center w-full h-full text-center border-2 lg:border-[6px] rounded-full cursor-not-allowed border-c-cyan'>
+          <p className='text-[8px] leading-[12px] px-2 text-white select-none sm:text-[10px] xl:text-xs uppercase '>{circulero?.apodo}</p>
         </div>
         :
         <div className='w-full p-1 md:p-[6px] bg-white'>
