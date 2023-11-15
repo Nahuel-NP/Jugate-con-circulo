d085c62816367686a6f8cb79099db329036eb586
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-11-15 12:01:40 -0300

    adding desktop responsive

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -3,7 +3,7 @@ import StarsContainer from "../components/StarsContainer";
 import { useGameStore } from "../store/gameStore";
 
 
-const AFTER_CLASSES = 'after:border lg:after:h-[70px] lg:after:bottom-0 lg:after:border-r md:after:border-0 after:border-c-yellow after:absolute  after:border-t-0 after:-bottom-4 after:left-0 after:w-full after:h-[140px]'
+const AFTER_CLASSES = 'after:border lg:after:h-[70px] lg:after:bottom-0 lg:after:border-r 2xl:after:border-r-2 md:after:border-0 after:border-c-yellow after:absolute  after:border-t-0 after:-bottom-4 after:left-0 after:w-full after:h-[140px]'
 const NTH_CLASSES = '[&:nth-child(10)]:col-span-3 [&:nth-child(11)]:col-span-3 lg:[&:nth-child(11)]:absolute lg:[&:nth-child(11)]:w-auto  lg:[&:nth-child(11)]:right-0 lg:[&:nth-child(11)]:top-1/2 lg:[&:nth-child(11)]:translate-x-1/2 lg:[&:nth-child(11)]:-translate-y-1/2 lg:[&:nth-child(10)]:col-span-1'
 
 const getRandomInt = (max: number): number => {
@@ -21,7 +21,7 @@ const Team = () => {
 
   return (
     <div className="relative bg-[url('/images/backgrounds/montana/front.webp')] px-4 bg-fixed bg-bottom bg-contain bg-no-repeat z-10   " style={{ viewTransitionName: 'view' }}>
-      <div className="container grid w-full gap-6 py-5 mx-auto max-w-7xl place-items-center md:grid-cols-2 lg:grid-cols-4 secure-min-h">
+      <div className="container grid w-full gap-6 py-5 mx-auto xl:max-w-7xl 2xl:max-w-[1366px] place-items-center md:grid-cols-2 lg:grid-cols-4 secure-min-h">
 
         <div className="flex flex-col gap-2 md:col-span-2 lg:hidden">
           <p className="text-3xl font-bold text-center text-c-yellow">{userName} creaste este <span className="italic">#EquipoEstrella</span> </p>
@@ -31,21 +31,21 @@ const Team = () => {
 
         <div className="lg:col-span-3 md:col-span-1">
 
-          <div className="relative flex-col hidden gap-2 p-6 lg:items-start lg:flex md:col-span-6 lg:col-span-5 lg:before:absolute lg:before:right-0 lg:before:border-r lg:before:bottom-0 lg:after:absolute lg:after:bottom-1/2 lg:after:w-16 lg:after:border-b lg:after:border-c-yellow lg:after:right-0 lg:before:h-1/2 lg:before:border-c-yellow">
-            <p className="text-3xl font-bold text-center text-c-yellow">{userName} creaste este <span className="italic">#EquipoEstrella</span> </p>
+          <div className="relative flex-col hidden gap-2 p-6 lg:items-start lg:flex md:col-span-6 lg:col-span-5 lg:before:absolute lg:before:right-0 lg:before:border-r 2xl:before:border-r-2 lg:before:bottom-0 lg:after:absolute lg:after:bottom-1/2 lg:after:w-16 lg:after:border-b 2xl:after:border-b-2 lg:after:border-c-yellow lg:after:right-0 lg:before:h-1/2 lg:before:border-c-yellow">
+            <p className="text-3xl 2xl:text-[32px] font-bold text-center text-c-yellow">{userName} creaste este <span className="italic">#EquipoEstrella</span> </p>
 
             <div className="flex items-center justify-center gap-4 ">
               <StarsContainer quantity={5} clases="max-w-[30px] md:max-w-[45px]" />
               <p className="text-3xl font-bold text-center text-c-yellow">{teamName}</p>
             </div>
           </div>
-          <div className={`relative lg:before:absolute lg:before:right-0 lg:before:border-r lg:before:h-16 lg:before:border-c-yellow grid grid-cols-6 gap-4 lg:col-span-3 md:border lg:pr-16 lg:border-r-0 md:border-t-0 md:p-4 md:border-c-yellow lg:grid-cols-5 md:max-w-xs lg:max-w-none md:gap-2 ${AFTER_CLASSES}`}>
+          <div className={`relative lg:before:absolute lg:before:right-0 lg:before:border-r 2xl:before:border-r-2 lg:before:h-16 lg:before:border-c-yellow grid grid-cols-6 gap-4 lg:col-span-3 md:border lg:pr-16 lg:border-r-0 md:border-t-0 2xl:border-2 2xl:border-t-0 2xl:border-r-0 md:p-4 md:border-c-yellow lg:grid-cols-5 md:max-w-xs lg:max-w-none md:gap-2 ${AFTER_CLASSES}`}>
             {
               team.map(partner => (
 
                 <div key={partner.rol}
                   className={` lg:col-span-1   w-full col-span-2 gap-2  p-1 md:p-[6px] flex flex-col justify-center items-center ${NTH_CLASSES}`}>
-                  <img className='p-2 rounded-full bg-gradient-to-r from-[#1DB7B3] to-[#9153C5] max-w-[100px] md:max-w-[80px]  ' src={`/images/circuleros/${partner.circulero.image ? partner.circulero.image : 'no-profile'}.jpg`} alt={partner.circulero.name} />
+                  <img className='p-2 rounded-full bg-gradient-to-r from-[#1DB7B3] to-[#9153C5] max-w-[100px] md:max-w-[80px] 2xl:max-w-[100px] ' src={`/images/circuleros/${partner.circulero.image ? partner.circulero.image : 'no-profile'}.jpg`} alt={partner.circulero.name} />
                   <p className="text-xs text-center text-white uppercase">{partner.rol}</p>
                 </div>
               ))
